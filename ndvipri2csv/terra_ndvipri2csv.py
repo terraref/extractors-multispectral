2909c5fd45862e74b7ff92d6581e5f948933e066
Max Burnette <mburnet88@gmail.com>
2017-05-23 11:25:31 -0500

    Update ndvipri2csv for terrautils

--- ndvipri2csv/terra_ndvipri2csv.py
+++ ndvipri2csv/terra_ndvipri2csv.py
@@ -20,6 +20,12 @@ class BinValues2Csv(Extractor):
     def __init__(self):
         Extractor.__init__(self)
 
+        influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
+        influx_port = os.getenv("INFLUXDB_PORT", 8086)
+        influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
+        influx_user = os.getenv("INFLUXDB_USER", "terra")
+        influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
+
         # add any additional arguments to parser
         # self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
         #                          help='maximum number (default=-1)')
@@ -29,15 +35,15 @@ class BinValues2Csv(Extractor):
         self.parser.add_argument('--overwrite', dest="force_overwrite", type=bool, nargs='?', default=False,
                                  help="whether to overwrite output file if it already exists in output directory")
         self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-                                 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+                                 default=influx_host, help="InfluxDB URL for logging")
         self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-                                 default=8086, help="InfluxDB port")
+                                 default=influx_port, help="InfluxDB port")
         self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-                                 default="terra", help="InfluxDB username")
+                                 default=influx_user, help="InfluxDB username")
         self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-                                 default="", help="InfluxDB password")
+                                 default=influx_pass, help="InfluxDB password")
         self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-                                 default="extractor_db", help="InfluxDB databast")
+                                 default=influx_db, help="InfluxDB database")
 
         # parse command line and load default logging configuration
         self.setup()
@@ -49,15 +55,18 @@ class BinValues2Csv(Extractor):
         # assign other arguments
         self.output_dir = self.args.output_dir
         self.force_overwrite = self.args.force_overwrite
-        self.influx_host = self.args.influx_host
-        self.influx_port = self.args.influx_port
-        self.influx_user = self.args.influx_user
-        self.influx_pass = self.args.influx_pass
-        self.influx_db = self.args.influx_db
+        self.influx_params = {
+            "host": self.args.influx_host,
+            "port": self.args.influx_port,
+            "db": self.args.influx_db,
+            "user": self.args.influx_user,
+            "pass": self.args.influx_pass
+        }
 
     def check_message(self, connector, host, secret_key, resource, parameters):
         # First, check if we have the correct sensor type
         md = pyclowder.datasets.download_metadata(connector, host, secret_key, resource['parent']['id'])
+        # TODO: Replace this with call to terrautils once available
         sensortype = self.determineSensorType(md)
         if sensortype in ["ndvi", "pri"]:
             for m in md:
@@ -69,8 +78,9 @@ class BinValues2Csv(Extractor):
 
             # Check if output already exists
             ds_info = pyclowder.datasets.get_info(connector, host, secret_key, resource['parent']['id'])
-            outPath = self.determineOutputDir(ds_info['name'])
-            if os.path.isfile(outPath) and not self.force_overwrite:
+            out_dir = terrautils.extractors.get_output_directory(self.output_dir, ds_info['name'], True)
+            out_file = os.path.join(out_dir, terrautils.extractors.get_output_filename(ds_info['name'], 'csv', opts=['extracted_values']))
+            if os.path.isfile(out_file) and not self.force_overwrite:
                 logging.info("skipping %s, outputs already exist" % resource['id'])
                 return CheckMessage.ignore
 
@@ -83,32 +93,33 @@ class BinValues2Csv(Extractor):
         created = 0
         bytes = 0
 
-        ds_info = pyclowder.datasets.get_info(connector, host, secret_key, resource['parent']['id'])
-        # Determine output file path
-        outPath = self.determineOutputDir(ds_info['name'])
         inPath = resource['local_paths'][0]
 
+        # Determine output file path
+        ds_info = pyclowder.datasets.get_info(connector, host, secret_key, resource['parent']['id'])
+        out_dir = terrautils.extractors.get_output_directory(self.output_dir, ds_info['name'], True)
+        out_file = os.path.join(out_dir, terrautils.extractors.get_output_filename(ds_info['name'], 'csv', opts=['extracted_values']))
         uploaded_file_ids = []
 
         # Extract NDVI values
-        if not os.path.isfile(outPath) or self.force_overwrite:
-            logging.info("...writing values to: %s" % outPath)
+        if not os.path.isfile(out_file) or self.force_overwrite:
+            logging.info("...writing values to: %s" % out_file)
             data = open(inPath, "rb").read()
             values = float(data[49:66])
-
-            with open(outPath,'wb') as csvfile:
+            data.close()
+            with open(out_file,'wb') as csvfile:
                 fields = ['file_name', 'NDVI'] # fields name for csv file
                 wr = csv.DictWriter(csvfile, fieldnames=fields, lineterminator = '\n')
                 wr.writeheader()
                 wr.writerow({'file_name':resource['name'], 'NDVI': values})
 
-            created += 1
-            bytes += os.path.getsize(outPath)
-
             fileid = pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], outPath)
             uploaded_file_ids.append(fileid)
+
+            created += 1
+            bytes += os.path.getsize(out_file)
         else:
-            logging.info("%s already exists; skipping %s" % (outPath, resource['id']))
+            logging.info("%s already exists; skipping %s" % (out_file, resource['id']))
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
         metadata = terrautils.extractors.build_metadata(host, self.extractor_info['name'], resource['id'], {
@@ -133,24 +144,6 @@ class BinValues2Csv(Extractor):
                         return "pri"
                     else:
                         return "unknown"
-
-    def determineOutputDir(self, ds_name):
-        if ds_name.find(" - ") > -1:
-            # sensor - timestamp
-            ds_name_parts = ds_name.split(" - ")
-            sensor_name = ds_name_parts[0]
-            if ds_name_parts[1].find("__") > -1:
-                # sensor - date__time
-                ds_time_parts = ds_name_parts[1].split("__")
-                timestamp = os.path.join(ds_time_parts[0], ds_name_parts[1])
-            else:
-                timestamp = ds_name_parts[1]
-            # /sensor/date/time
-            subPath = os.path.join(sensor_name, timestamp)
-        else:
-            subPath = ds_name
-
-        return os.path.join(self.output_dir, subPath, "extracted_values.csv")
 
 if __name__ == "__main__":
     extractor = BinValues2Csv()
