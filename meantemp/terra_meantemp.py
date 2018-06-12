05b65a9dbcbc808e9f516ee9157a8751af570d6e
Max Burnette <mburnet2@illinois.com>
2018-06-12 14:42:47 -0500

    route CSVs to bety and geostreams extractors

--- meantemp/terra_meantemp.py
+++ meantemp/terra_meantemp.py
@@ -9,10 +9,9 @@ import re
 from pyclowder.utils import CheckMessage
 from pyclowder.datasets import download_metadata, upload_metadata, get_info
 from terrautils.extractors import TerrarefExtractor, is_latest_file, \
-    build_dataset_hierarchy, build_metadata, load_json_file
+    build_dataset_hierarchy, build_metadata, load_json_file, upload_to_dataset
 from terrautils.gdal import centroid_from_geojson, clip_raster
 from terrautils.betydb import add_arguments, submit_traits, get_site_boundaries
-from terrautils.geostreams import create_datapoint_with_dependencies
 
 
 logging.basicConfig(format='%(asctime)s %(message)s')
@@ -77,21 +76,34 @@ class FlirMeanTemp(TerrarefExtractor):
         # Get full list of experiment plots using date as filter
         ds_info = get_info(connector, host, secret_key, resource['parent']['id'])
         timestamp = ds_info['name'].split(" - ")[1]
-        all_plots = get_site_boundaries(timestamp, city='Maricopa')
+        time_fmt = timestamp+"T12:00:00-07:00"
+        rootdir = self.sensors.create_sensor_path(timestamp, sensor="fullfield", ext=".csv")
+        out_csv = os.path.join(os.path.dirname(rootdir),
+                               resource['name'].replace(".tif", "_meantemp_bety.csv"))
+        out_geo = os.path.join(os.path.dirname(rootdir),
+                               resource['name'].replace(".tif", "_meantemp_geo.csv"))
 
-        csv_path = self.sensors.create_sensor_path(timestamp)
-        csv_file = open(csv_path, 'w')
+        # TODO: What should happen if CSV already exists? If we're here, there's no completed metadata...
+
+        self.log_info(resource, "Writing BETY CSV to %s" % out_csv)
+        csv_file = open(out_csv, 'w')
         (fields, traits) = get_traits_table()
         csv_file.write(','.join(map(str, fields)) + '\n')
 
+        self.log_info(resource, "Writing Geostreams CSV to %s" % out_geo)
+        geo_file = open(out_geo, 'w')
+        geo_file.write(','.join(['trait', 'lat', 'lon', 'dp_time', 'source', 'value', 'timestamp']) + '\n')
+
         successful_plots = 0
         nan_plots = 0
+        all_plots = get_site_boundaries(timestamp, city='Maricopa')
         for plotname in all_plots:
             if plotname.find("KSU") > -1:
                 self.log_info(resource, "skipping %s" % plotname)
                 continue
+
             bounds = all_plots[plotname]
-            self.log_info(resource, "clipping and processing %s" % plotname)
+            centroid_lonlat = json.loads(centroid_from_geojson(bounds))["coordinates"]
 
             # Use GeoJSON string to clip full field to this plot
             (pxarray, geotrans) = clip_raster(resource['local_paths'][0], bounds)
@@ -102,23 +114,19 @@ class FlirMeanTemp(TerrarefExtractor):
 
             # Create BETY-ready CSV
             if not numpy.isnan(mean_tc):
+                geo_file.write(','.join(['IR Surface Temperature',
+                                         str(centroid_lonlat[1]),
+                                         str(centroid_lonlat[0]),
+                                         time_fmt,
+                                         host + ("" if host.endswith("/") else "/") + "files/" + resource['id'],
+                                         str(mean_tc),
+                                         timestamp]) + '\n')
+
                 traits['surface_temperature'] = str(mean_tc)
                 traits['site'] = plotname
                 traits['local_datetime'] = timestamp+"T12:00:00"
                 trait_list = generate_traits_list(traits)
-                #generate_csv(tmp_csv, fields, trait_list)
                 csv_file.write(','.join(map(str, trait_list)) + '\n')
-
-                # Prepare and submit datapoint
-                centroid_lonlat = json.loads(centroid_from_geojson(bounds))["coordinates"]
-                time_fmt = timestamp+"T12:00:00-07:00"
-                dpmetadata = {
-                    "source": host + ("" if host.endswith("/") else "/") + "files/" + resource['id'],
-                    "surface_temperature": str(mean_tc)
-                }
-                create_datapoint_with_dependencies(connector, host, secret_key, "IR Surface Temperature",
-                                                   (centroid_lonlat[1], centroid_lonlat[0]), time_fmt, time_fmt,
-                                                   dpmetadata, timestamp)
             else:
                 nan_plots += 1
 
@@ -128,8 +136,11 @@ class FlirMeanTemp(TerrarefExtractor):
 
         # submit CSV to BETY
         csv_file.close()
-        self.log_info(resource, "submitting %s to BETYdb" % csv_path)
-        submit_traits(csv_path, betykey=self.bety_key)
+        geo_file.close()
+
+        # Upload CSVs to Clowder
+        fileid = upload_to_dataset(connector, host, self.clowder_user, self.clowder_pass, resource['parent']['id'], out_csv)
+        geoid  = upload_to_dataset(connector, host, self.clowder_user, self.clowder_pass, resource['parent']['id'], out_geo)
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
         self.log_info(resource, "uploading metadata to dataset")
@@ -137,11 +148,16 @@ class FlirMeanTemp(TerrarefExtractor):
             "total_plots": len(all_plots),
             "plots_processed": successful_plots,
             "blank_plots": nan_plots,
+            "files_created": [fileid, geoid],
             "betydb_link": "https://terraref.ncsa.illinois.edu/bety/api/beta/variables?name=surface_temperature"
         }, 'dataset')
         upload_metadata(connector, host, secret_key, resource['parent']['id'], metadata)
 
+        # Trigger downstream extractors
+
         self.end_message(resource)
+        submit_extraction(connector, host, secret_key, fileid, "terra.betydb")
+        submit_extraction(connector, host, secret_key, geoid, "terra.geostreams")
 
 if __name__ == "__main__":
     extractor = FlirMeanTemp()
