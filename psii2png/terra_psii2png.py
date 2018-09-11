6910964de6d6579b521db06538ec1d6bd509cdf6
Max Burnette <mburnet2@illinois.com>
2018-09-11 09:45:02 -0500

    add hooks for geotiff generation

--- psii2png/terra_psii2png.py
+++ psii2png/terra_psii2png.py
@@ -2,7 +2,9 @@
 
 import os
 import logging
-import numpy
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
 
 from pyclowder.utils import CheckMessage
 from pyclowder.files import upload_to_dataset
@@ -10,8 +12,8 @@ from pyclowder.datasets import download_metadata, upload_metadata
 from terrautils.extractors import TerrarefExtractor, is_latest_file, load_json_file, create_image, \
     build_metadata, build_dataset_hierarchy
 from terraref.metadata import get_extractor_metadata, get_terraref_metadata
-
-import PSII_analysis as psiiCore
+from terrautils.formats import create_geotiff
+from terrautils.spatial import geojson_to_tuples
 
 
 class PSIIBin2Png(TerrarefExtractor):
@@ -21,11 +23,65 @@ class PSIIBin2Png(TerrarefExtractor):
         # parse command line and load default logging configuration
         self.setup(sensor='ps2_png')
 
+    def load_png(self, file_path, height, width):
+        """Load PNG image into a numpy array"""
+        im = Image.open(file_path)
+        return np.array(im).astype('uint8')
+
+    def analyze(self, frames, hist_path, coloredImg_path):
+
+        # TODO: Where do these dimensions come from?
+        img_width = 1936
+        img_height = 1216
+
+        fdark = self.load_png(frames[0], img_height, img_width)
+        fmin = self.load_png(frames[1], img_height, img_width)
+
+        # Calculate the maximum fluorescence for each frame
+        fave = []
+        fave.append(np.max(fdark))
+        # Calculate the maximum value for frames 2 through 100. Bin file 101 is an XML file that lists the frame times
+        for i in range(2, 101):
+            img = self.load_png(frames[i], img_height, img_width)
+            fave.append(np.max(img))
+
+        # Assign the first image with the most fluorescence as F-max
+        fmax = self.load_png(frames[np.where(fave == np.max(fave))[0][0]], img_height, img_width)
+        # Calculate F-variable (F-max - F-min)
+        fv = np.subtract(fmax, fmin)
+        # Calculate Fv/Fm (F-variable / F-max)
+        fvfm = np.divide(fv.astype('float'), fmax.astype('float'))
+        # Fv/Fm will generate invalid values, such as division by zero
+        # Convert invalid values to zero. Valid values will be between 0 and 1
+        fvfm[np.where(np.isnan(fvfm))] = 0
+        fvfm[np.where(np.isinf(fvfm))] = 0
+        fvfm[np.where(fvfm > 1.0)] = 0
+
+        # Plot Fv/Fm (pseudocolored)
+        plt.imshow(fvfm, cmap="viridis")
+        plt.savefig(coloredImg_path)
+        plt.show()
+        plt.close()
+
+        # Calculate histogram of Fv/Fm values from the whole image
+        hist, bins = np.histogram(fvfm, bins=20)
+        # Plot Fv/Fm histogram
+        width = 0.7 * (bins[1] - bins[0])
+        center = (bins[:-1] + bins[1:]) / 2
+        plt.bar(center, hist, align='center', width=width)
+        plt.xlabel("Fv/Fm")
+        plt.ylabel("Pixels")
+        plt.show()
+        plt.savefig(hist_path)
+        plt.close()
+
     def check_message(self, connector, host, secret_key, resource, parameters):
         # Check for 0000-0101 bin files before beginning processing
         if len(resource['files']) < 102:
+            self.log_skip(resource, "less than 102 files found")
             return CheckMessage.ignore
         if not is_latest_file(resource):
+            self.log_skip(resource, "not latest file")
             return CheckMessage.ignore
 
         timestamp = resource['dataset_info']['name'].split(" - ")[1]
@@ -47,24 +103,26 @@ class PSIIBin2Png(TerrarefExtractor):
 
         # Do the outputs already exist?
         if ind_output == 102 and os.path.exists(hist_path) and os.path.exists(coloredImg_path):
-            logging.info("skipping dataset %s, outputs already exist" % resource['id'])
+            self.log_skip(resource, "outputs already exist")
             return CheckMessage.ignore
         # Do we have too few input BIN files?
         if ind_add < 102:
+            self.log_skip(resource, "less than 102 .bin files found")
             return CheckMessage.ignore
 
+        # Check metadata to verify we have what we need
         md = download_metadata(connector, host, secret_key, resource['id'])
         if get_extractor_metadata(md, self.extractor_info['name']) and not self.overwrite:
-            logging.info("skipping dataset %s, found existing metadata" % resource['id'])
+            self.log_skip(resource, "metadata indicates it was already processed")
             return CheckMessage.ignore
-
         if get_terraref_metadata(md):
             return CheckMessage.download
-
-        return CheckMessage.ignore
+        else:
+            self.log_skip(resource, "no terraref metadata found")
+            return CheckMessage.ignore
 
     def process_message(self, connector, host, secret_key, resource, parameters):
-        self.start_message()
+        self.start_message(resource)
 
         # Get bin files and metadata
         metadata = None
@@ -83,7 +141,7 @@ class PSIIBin2Png(TerrarefExtractor):
                 if f['filename'].endswith(format_ind+'.bin'):
                     frames[ind] = f['filename']
         if None in [metadata] or len(frames) < 101:
-            logging.error('could not find all of frames/metadata')
+            self.log_error(resource, 'could not find all of frames/metadata')
             return
 
         # Determine output directory
@@ -98,16 +156,23 @@ class PSIIBin2Png(TerrarefExtractor):
 
         img_width = 1936
         img_height = 1216
+
+        gps_bounds = geojson_to_tuples(metadata['spatial_metadata']['ps2Top']['bounding_box'])
+
         png_frames = {}
         # skip 0101.bin since 101 is an XML file that lists the frame times
         for ind in range(0, 101):
             format_ind = "{0:0>4}".format(ind) # e.g. 1 becomes 0001
             png_path = self.sensors.create_sensor_path(timestamp, opts=[format_ind])
+            tif_path = png_path.replace(".png", ".tif")
             png_frames[ind] = png_path
             if not os.path.exists(png_path) or self.overwrite:
-                logging.info("...generating and uploading %s" % png_path)
-                pixels = numpy.fromfile(frames[ind], numpy.dtype('uint8')).reshape([img_height, img_width])
+                self.log_info(resource, "generating and uploading %s" % png_path)
+                pixels = np.fromfile(frames[ind], np.dtype('uint8')).reshape([img_height, img_width])
+
                 create_image(pixels, png_path)
+                create_geotiff(pixels, gps_bounds, tif_path, None, False, self.extractor_info, metadata)
+
                 if png_path not in resource['local_paths']:
                     fileid = upload_to_dataset(connector, host, secret_key, target_dsid, png_path)
                     uploaded_file_ids.append(fileid)
@@ -115,9 +180,9 @@ class PSIIBin2Png(TerrarefExtractor):
                 self.bytes += os.path.getsize(png_path)
 
         # Generate aggregate outputs
-        logging.info("...generating aggregates")
+        self.log_info(resource, "generating aggregates")
         if not (os.path.exists(hist_path) and os.path.exists(coloredImg_path)) or self.overwrite:
-            psiiCore.psii_analysis(png_frames, hist_path, coloredImg_path)
+            self.analyze(png_frames, hist_path, coloredImg_path)
             self.created += 2
             self.bytes += os.path.getsize(hist_path)
             self.bytes += os.path.getsize(coloredImg_path)
@@ -131,9 +196,10 @@ class PSIIBin2Png(TerrarefExtractor):
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
         metadata = build_metadata(host, self.extractor_info, target_dsid, {
                                   "files_created": uploaded_file_ids}, 'dataset')
+        self.log_info(resource, "uploading extractor metadata")
         upload_metadata(connector, host, secret_key, resource['id'], metadata)
 
-        self.end_message()
+        self.end_message(resource)
 
 if __name__ == "__main__":
     extractor = PSIIBin2Png()
