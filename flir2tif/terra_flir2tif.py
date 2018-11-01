1a927192339b9651aebc1ab983a061164e165300
Max Burnette <mburnet2@illinois.com>
2018-11-01 09:45:45 -0500

    reprocessing prep overhauls

--- flir2tif/terra_flir2tif.py
+++ flir2tif/terra_flir2tif.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 import os
-import logging
 import shutil
 import numpy
 import tempfile
 
 from pyclowder.utils import CheckMessage
 from pyclowder.files import upload_to_dataset
-from pyclowder.datasets import download_metadata, upload_metadata
+from pyclowder.datasets import download_metadata, upload_metadata, remove_metadata
 from terrautils.metadata import get_extractor_metadata, get_terraref_metadata, calculate_scan_time
-from terrautils.extractors import TerrarefExtractor, is_latest_file, \
-    build_dataset_hierarchy, build_metadata, load_json_file, file_exists
+from terrautils.extractors import TerrarefExtractor, is_latest_file, delete_file, \
+    build_dataset_hierarchy, build_metadata, load_json_file, file_exists, contains_required_files
 from terrautils.formats import create_geotiff, create_image
 from terrautils.spatial import geojson_to_tuples
 
@@ -43,73 +42,74 @@ class FlirBin2JpgTiff(TerrarefExtractor):
             self.log_skip(resource, "not latest file")
             return CheckMessage.ignore
 
-        # Check for an ir.BIN file before beginning processing
-        found_ir = None
-        for f in resource['files']:
-            if 'filename' in f and f['filename'].endswith('_ir.bin'):
-                found_ir = f['filepath']
-        if not (found_ir):
-            self.log_skip(resource, "IR bin file not found")
+        # Check for an _ir.bin file before beginning processing
+        if not contains_required_files(resource, ['_ir.bin']):
+            self.log_skip(resource, "missing required files")
             return CheckMessage.ignore
-
-        # Check if outputs already exist
-        if not self.overwrite:
-            timestamp = resource['dataset_info']['name'].split(" - ")[1]
-            png_path = self.sensors.get_sensor_path(timestamp, ext='png')
-            tiff_path = self.sensors.get_sensor_path(timestamp)
-
-            if file_exists(png_path) and file_exists(tiff_path) and not self.overwrite:
-                self.log_skip(resource, "outputs found in %s" % out_dir)
-                return CheckMessage.ignore
 
         # Check metadata to verify we have what we need
         md = download_metadata(connector, host, secret_key, resource['id'])
-        if get_extractor_metadata(md, self.extractor_info['name']) and not self.overwrite:
-            self.log_skip("metadata indicates it was already processed")
-            return CheckMessage.ignore
         if get_terraref_metadata(md):
+            if get_extractor_metadata(md, self.extractor_info['name'], self.extractor_info['version']):
+                # Make sure outputs properly exist
+                tif = sensor.get_sensor_path(timestamp, sensor="flirIrCamera")
+                png = tif.replace(".tif", ".png")
+                if file_exists(png) and file_exists(tif):
+                    self.log_skip(resource, "metadata v%s and outputs already exist" % self.extractor_info['version'])
+                    return CheckMessage.ignore
+            # Have TERRA-REF metadata, but not any from this extractor
             return CheckMessage.download
         else:
-            self.log_skip("no terraref metadata found")
+            self.log_skip(resource, "no terraref metadata found")
             return CheckMessage.ignore
 
     def process_message(self, connector, host, secret_key, resource, parameters):
         self.start_message(resource)
 
         # Get BIN file and metadata
-        bin_file, metadata = None, None
+        bin_file, terra_md_full = None, None
         for f in resource['local_paths']:
             # First check metadata attached to dataset in Clowder for item of interest
             if f.endswith('_dataset_metadata.json'):
                 all_dsmd = load_json_file(f)
-                metadata = get_terraref_metadata(all_dsmd, 'flirIrCamera')
+                terra_md_full = get_terraref_metadata(all_dsmd, 'flirIrCamera')
             # Otherwise, check if metadata was uploaded as a .json file
             elif f.endswith('_ir.bin'):
                 bin_file = f
-        if None in [bin_file, metadata]:
-            self.log_error(resource, "could not locate each of ir+metadata in processing")
+        if None in [bin_file, terra_md_full]:
             raise ValueError("could not locate each of ir+metadata in processing")
+
+        # Fetch experiment name from terra metadata
+        if 'experiment_metadata' in terra_md_full and 'name' in terra_md_full['experiment_metadata']:
+            experiment_name = terra_md_full['experiment_metadata']['name']
+            if ":" in experiment_name:
+                season_name = experiment_name.split(": ")[0]
+                experiment_name = experiment_name.split(": ")[1]
+            else:
+                season_name = None
+        else:
+            season_name = None
+            experiment_name = None
 
         # Determine output directory
         timestamp = resource['dataset_info']['name'].split(" - ")[1]
-        png_path = self.sensors.create_sensor_path(timestamp, ext='png')
         tiff_path = self.sensors.create_sensor_path(timestamp)
-        uploaded_file_ids = []
+        png_path = tiff_path.replace(".tif", ".png")
 
         target_dsid = build_dataset_hierarchy(host, secret_key, self.clowder_user, self.clowder_pass, self.clowderspace,
-                                              self.sensors.get_display_name(),
+                                              season_name, experiment_name, self.sensors.get_display_name(),
                                               timestamp[:4], timestamp[5:7], timestamp[8:10],
                                               leaf_ds_name=self.sensors.get_display_name()+' - '+timestamp)
+        uploaded_file_ids = []
 
-        # Upload original Lemnatec metadata to new Level_1 dataset
-        md = get_terraref_metadata(all_dsmd)
-        md['raw_data_source'] = host + ("" if host.endswith("/") else "/") + "datasets/" + resource['id']
-        lemna_md = build_metadata(host, self.extractor_info, target_dsid, md, 'dataset')
-        self.log_info(resource, "uploading LemnaTec metadata")
-        upload_metadata(connector, host, secret_key, target_dsid, lemna_md)
+        self.log_info(resource, "uploading LemnaTec metadata to ds [%s]" % target_dsid)
+        remove_metadata(connector, host, secret_key, target_dsid, self.extractor_info['name'])
+        terra_md_trim = get_terraref_metadata(all_dsmd)
+        terra_md_trim['raw_data_source'] = host + ("" if host.endswith("/") else "/") + "datasets/" + resource['id']
+        level1_md = build_metadata(host, self.extractor_info, target_dsid, terra_md_trim, 'dataset')
+        upload_metadata(connector, host, secret_key, target_dsid, level1_md)
 
         skipped_png = False
-
         if not file_exists(png_path) or self.overwrite:
             self.log_info(resource, "creating & uploading %s" % png_path)
 
@@ -119,7 +119,12 @@ class FlirBin2JpgTiff(TerrarefExtractor):
             create_image(raw_data, png_path, self.scale_values)
 
             # Only upload the newly generated file to Clowder if it isn't already in dataset
-            if png_path not in resource["local_paths"]:
+            if png_path not in resource["local_paths"] or self.overwrite:
+                if png_path in resource["local_paths"]:
+                    for targ_file in resource["files"]:
+                        if targ_file['filename'] == os.path.basename(png_path):
+                            self.log_info(resource, "Deleting file %s [%s]" % (targ_file['filename'], targ_file['id']))
+                            delete_file(host, secret_key, targ_file['id'])
                 fileid = upload_to_dataset(connector, host, secret_key, target_dsid, png_path)
                 uploaded_file_ids.append(host + ("" if host.endswith("/") else "/") + "files/" + fileid)
             self.created += 1
@@ -129,28 +134,35 @@ class FlirBin2JpgTiff(TerrarefExtractor):
 
         if not file_exists(tiff_path) or self.overwrite:
             self.log_info(resource, "generating temperature matrix")
-            gps_bounds = geojson_to_tuples(metadata['spatial_metadata']['flirIrCamera']['bounding_box'])
+            gps_bounds = geojson_to_tuples(terra_md_full['spatial_metadata']['flirIrCamera']['bounding_box'])
             if skipped_png:
                 raw_data = numpy.fromfile(bin_file, numpy.dtype('<u2')).reshape([480, 640]).astype('float')
                 raw_data = numpy.rot90(raw_data, 3)
-            tc = getFlir.rawData_to_temperature(raw_data, metadata) # get temperature
+            tc = getFlir.rawData_to_temperature(raw_data, terra_md_full) # get temperature
 
             self.log_info(resource, "creating & uploading %s" % tiff_path)
             # Rename temporary tif after creation to avoid long path errors
             out_tmp_tiff = os.path.join(tempfile.gettempdir(), resource['id'].encode('utf8'))
-            create_geotiff(tc, gps_bounds, out_tmp_tiff, None, True, self.extractor_info, metadata)
+            create_geotiff(tc, gps_bounds, out_tmp_tiff, None, True, self.extractor_info, terra_md_full)
             shutil.move(out_tmp_tiff, tiff_path)
-            if tiff_path not in resource["local_paths"]:
+            if tiff_path not in resource["local_paths"] or self.overwrite:
+                if tiff_path in resource["local_paths"]:
+                    for targ_file in resource["files"]:
+                        if targ_file['filename'] == os.path.basename(tiff_path):
+                            self.log_info(resource, "Deleting file %s [%s]" % (targ_file['filename'], targ_file['id']))
+                            delete_file(host, secret_key, targ_file['id'])
                 fileid = upload_to_dataset(connector, host, secret_key, target_dsid, tiff_path)
                 uploaded_file_ids.append(host + ("" if host.endswith("/") else "/") + "files/" + fileid)
             self.created += 1
             self.bytes += os.path.getsize(tiff_path)
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
-        metadata = build_metadata(host, self.extractor_info, target_dsid, {
-            "files_created": uploaded_file_ids}, 'dataset')
-        self.log_info(resource, "uploading extractor metadata")
-        upload_metadata(connector, host, secret_key, resource['id'], metadata)
+        extractor_md = build_metadata(host, self.extractor_info, target_dsid, {
+            "files_created": uploaded_file_ids
+        }, 'dataset')
+        self.log_info(resource, "uploading extractor metadata to raw dataset")
+        remove_metadata(connector, host, secret_key, resource['id'], self.extractor_info['name'])
+        upload_metadata(connector, host, secret_key, resource['id'], extractor_md)
 
         self.end_message(resource)
 
